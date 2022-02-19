4789b3a82171c4fe7499db4e2da87d46aad656a7
zhumgal.akaev.kg <jumgal.akaev@zensoft.io>
2022-02-19 19:54:33 +0600

    removed redundant imports

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Header from './components/Header/Header';
 import Catalog from './components/main/section-2/Section-Two';
 // import Search from './components/main/search';
 import FreshCoffee from './components/main/section-1/1.1/FreshCoffee';
-import SalesGoods from './components/main/section-2/SalesGoods';
-import TeaAndCoffee from './components/main/section-1/1.2/tea and coffee';
 import Slider from './components/main/section-1/1/slider';
 
 function App() {
